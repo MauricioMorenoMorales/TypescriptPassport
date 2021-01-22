9b9f88614863897455c2372bc02a29458850ba7e
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-01-22 12:05:28 -0600

    Testing config file in gitignore

--- dist/config/config.js
+++ dist/config/config.js
@@ -6,12 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({
-    path: path_1.default.resolve(__dirname, '../.env'),
+    path: path_1.default.resolve(__dirname, '../../.env'),
 });
 exports.default = {
     DB: {
-        USER: process.env.MONGODB_USER,
-        PASSWORD: process.env.MONGODB_PASSWORD,
-        DBNAME: process.env.MONGODB_DBNAME,
+        USER: process.env.MONGODB_USER || 'db_user_otro',
+        PASSWORD: process.env.MONGODB_PASSWORD || '12345',
+        DBNAME: process.env.MONGODB_DBNAME || 'typescriptPassport',
     },
 };
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config({
-	path: path.resolve(__dirname, '../.env'),
+	path: path.resolve(__dirname, '../../.env'),
 })
 
 export default {
 	DB: {
-		USER: process.env.MONGODB_USER,
-		PASSWORD: process.env.MONGODB_PASSWORD,
-		DBNAME: process.env.MONGODB_DBNAME,
+		USER: process.env.MONGODB_USER || 'db_user_otro',
+		PASSWORD: process.env.MONGODB_PASSWORD || '12345',
+		DBNAME: process.env.MONGODB_DBNAME || 'typescriptPassport',
 	},
 }
