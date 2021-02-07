b43cbff1d3aee752a9044927ae10db010442cfa8
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-06 23:28:42 -0600

    signin and signup completed

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -1,11 +1,54 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signIn = exports.signUp = void 0;
-const signUp = (req, res) => {
-    res.send('signup');
-};
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const user_1 = __importDefault(require("../models/user"));
+const config_1 = __importDefault(require("../config"));
+function createToken(user) {
+    return jsonwebtoken_1.default.sign({ id: user.id, email: user.email }, config_1.default.JWTSECRET);
+}
+const signUp = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    if (!email || !password)
+        return res
+            .status(400)
+            .json({ message: 'Please send your email and password' });
+    const user = yield user_1.default.findOne({ email });
+    if (user)
+        return res
+            .status(400)
+            .json({ message: 'The user already exists try creating a new one' });
+    const newUser = new user_1.default({ email, password });
+    yield newUser.save();
+    return res.status(201).json({ message: 'User created' });
+});
 exports.signUp = signUp;
-const signIn = (req, res) => {
-    res.send('signin');
-};
+const signIn = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    if (!email || !password)
+        return res
+            .status(400)
+            .json({ message: 'Please send your email and password' });
+    const user = yield user_1.default.findOne({ email });
+    if (!user)
+        return res.status(400).json({ message: 'This user does not exist' });
+    const isMatch = yield user.comparePassword(password);
+    if (isMatch)
+        return res.status(200).json({ token: createToken(user) });
+    return res
+        .status(400)
+        .json({ message: 'The email or password are incorrect' });
+});
 exports.signIn = signIn;
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,41 @@
-import { RequestHandler } from 'express'
+import { RequestHandler, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import UserModel, { IUser } from '../models/user'
+import config from '../config'
 
-export const signUp: RequestHandler = (req, res) => {
-	res.send('signup')
+function createToken(user: IUser) {
+	return jwt.sign({ id: user.id, email: user.email }, config.JWTSECRET)
 }
-export const signIn: RequestHandler = (req, res) => {
-	res.send('signin')
+
+export const signUp: RequestHandler = async (req, res): Promise<Response> => {
+	const { email, password } = req.body
+	if (!email || !password)
+		return res
+			.status(400)
+			.json({ message: 'Please send your email and password' })
+	const user = await UserModel.findOne({ email })
+	if (user)
+		return res
+			.status(400)
+			.json({ message: 'The user already exists try creating a new one' })
+	const newUser = new UserModel({ email, password })
+	await newUser.save()
+	return res.status(201).json({ message: 'User created' })
+}
+
+export const signIn: RequestHandler = async (req, res): Promise<Response> => {
+	const { email, password } = req.body
+	if (!email || !password)
+		return res
+			.status(400)
+			.json({ message: 'Please send your email and password' })
+	const user: IUser = await UserModel.findOne({ email })
+	if (!user)
+		return res.status(400).json({ message: 'This user does not exist' })
+	const isMatch = await user.comparePassword(password)
+	if (isMatch) return res.status(200).json({ token: createToken(user) })
+
+	return res
+		.status(400)
+		.json({ message: 'The email or password are incorrect' })
 }
